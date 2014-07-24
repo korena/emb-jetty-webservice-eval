fbddf9c01920bfc214e10616623190cde88d21fb
moaz korena <moazkorena@gmail.com>
2014-07-25 06:47:33 +0800

    Initial POM.xml

---
+++ src/main/java/com/app/injectedIn/RecieveInjection.java
@@ -0,0 +1,11 @@
+package com.app.injectedIn;
+
+/**
+ * Created by korena on 7/25/14.
+ */
+public class RecieveInjection {
+
+
+
+
+}
---
+++ src/main/java/com/app/injectees/Injectable.java
@@ -0,0 +1,7 @@
+package com.app.injectees;
+
+/**
+ * Created by korena on 7/25/14.
+ */
+public class Injectable {
+}
